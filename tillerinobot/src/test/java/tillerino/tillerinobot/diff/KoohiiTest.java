cd971632bbb537d46d3ac7d33a98273879d3ee97
Tillerino <tillmann.gaida@gmail.com>
2018-01-16 14:52:23 +0100

    Unit test for #137

--- tillerinobot/src/test/java/tillerino/tillerinobot/diff/KoohiiTest.java
+++ tillerinobot/src/test/java/tillerino/tillerinobot/diff/KoohiiTest.java
@@ -6,6 +6,7 @@ import java.io.InputStream;
 import java.util.Collections;
 
 import org.junit.Test;
+import org.tillerino.osuApiModel.OsuApiBeatmap;
 
 public class KoohiiTest {
 	@Test
@@ -18,6 +19,22 @@ public class KoohiiTest {
 			assertEquals(313, diff.getAllObjectsCount());
 			assertEquals(229, diff.getCircleCount());
 			assertEquals(404, diff.getMaxCombo());
+		}
+	}
+
+	@Test
+	public void testIssue137() throws Exception {
+		try (InputStream is = ClassLoader.getSystemResourceAsStream(
+				"Ayase Rie - Yuima-ruWorld TVver. (Fycho) [Extra].osu")) {
+			DifficultyProperties diff = new Koohii().calculate(is, Collections.emptyList());
+			OsuApiBeatmap beatmap = new OsuApiBeatmap();
+			beatmap.setOverallDifficulty(8.7);
+			beatmap.setApproachRate(9);
+			beatmap.setMaxCombo(239);
+			CBeatmapImpl impl = new CBeatmapImpl(beatmap, diff.getSpeed(), diff.getAim(), diff.getCircleCount(),
+					diff.getAllObjectsCount(), true, false, true);
+			CStandardScore score = new CStandardScore(239, 165, 13, 0, 0, 0);
+			assertEquals(138.287, score.getPP(impl), 0d);
 		}
 	}
 }
